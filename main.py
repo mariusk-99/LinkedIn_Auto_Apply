bdba9470a6ec451571a88f8a47d8e5ea1471b637
mk-hack-18 <68790682+mk-hack-18@users.noreply.github.com>
2023-05-08 23:33:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChain
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 
 import json
 import time
 import random
+import re
 
 class EasyApplyLinkedIn:
 
@@ -33,8 +35,6 @@ class EasyApplyLinkedIn:
         # self.chrome_options.add_argument("--start-fullscreen")
         
         # Method 2: Maximising the tab
-
-        self.driver.maximize_window()
 
 
     def login_linkedin(self):
@@ -162,13 +162,47 @@ class EasyApplyLinkedIn:
         time.sleep(2)
 
         current_page = self.driver.current_url
-        job_listings = self.driver.find_element(By.XPATH, "//li[starts-with(@class, 'ember-view   jobs-search-results__list-item occludable-update p0 relative scaffold-layout__list-item')]")
+        job_listings = self.driver.find_elements(By.XPATH, "//li[starts-with(@class, 'ember-view   jobs-search-results__list-item occludable-update p0 relative scaffold-layout__list-item')]")
 
         # for each job add, submits and application if no questions are asked
 
         for job_listing in job_listings:
             hover = ActionChains(self.driver).move_to_element(job_listing)
             hover.perform()
+            titles = job_listing.find_elements(By.CLASS_NAME, 'disabled ember-view job-card-container__link job-card-list__title')
+            for title in titles:
+                self.submit_application(title)
+
+        # if there is more than one page, find the pages and apply to teh results of each page
+
+        if total_results_int > 24:
+            time.sleep(2)
+
+            #find the last page and construct url of each page based on the total amount of pages
+            find_pages = self.driver.find_elements(By.CLASS_NAME, 'artdeco-pagination__indicator artdeco-pagination__indicator--number ember-view')
+            total_pages = find_pages(len(find_pages)-1).text
+            total_pages_int = int(re.sub(r"[^\d.]","", total_pages))
+            get_last_page = self.driver.find_element(By.XPATH, "//button[@aria-label='Page "+str(total_pages_int)+"']")
+            get_last_page.send_keys(Keys.RETURN)
+            time.sleep(2)
+            last_page = self.driver.current_url
+            total_jobs = int(last_page.split('start=', 1)[1])
+
+            # go through all available pages and job offers and apply
+            for page_number in range(25, total_jobs+25, 25):
+                self.driver.get(current_page+"&start="+str(page_number))
+                time.sleep(2)
+                results_ext = self.driver.find_elements(By.XPATH, "//li[starts-with(@class, 'ember-view   jobs-search-results__list-item occludable-update p0 relative scaffold-layout__list-item')]")
+                for result_ext in results_ext:
+                    hover_ext = ActionChains(self.driver).move_to_element(result_ext)
+                    hover_ext.perform()
+                    titles_ext = result_ext.find_elements(By.CLASS_NAME, 'disabled ember-view job-card-container__link job-card-list__title')
+                    for title_ext in titles_ext:
+                        self.submit_application(title_ext)
+
+        else:
+            self.close_session()
+                    
         
     def submit_application(self,job_ad):
         """This function submits the application for the job ad found"""
@@ -184,26 +218,54 @@ class EasyApplyLinkedIn:
 
         except NoSuchElementException:
             print('You already applied to this job, go to the next one ...')
-        
+            pass
         time.sleep(1)
 
         # try to submit application if the application is available
-        #try:
-            #submit = self.driver.find_element(By.XPATH, "")
+        try:
+            submit = self.driver.find_element(By.XPATH, "//button[@aria-label='Submit application']")
+            submit.send_keys(Keys.RETURN)
+        except NoSuchElementException:
+            print('Not direct application, going to next...')
+
+            try:
+                discard = self.driver.find_element(By.XPATH, "//button[@data-test-modal-close-btn]")
+                discard.send_keys(Keys.RETURN)
+                time.sleep(1)
+                discard_confirm = self.driver.find_element(By.XPATH, "//button[@data-test-dialog-secondary-btn]")
+                discard_confirm.send_keys(Keys.RETURN)
+                time.sleep(1)
+            except NoSuchElementException:
+                pass
 
 
+        time.sleep(1)
+
+    def close_session(self):
+        """This function closes the actual session"""
+
+        print('End of the session, see you later')
+        self.driver.close()
 
 
+    def apply(self):
+        """Apply to jobs"""
+
+        self.driver.maximize_window()
+        self.login_linkedin()
+        time.sleep(round(random.uniform(2,3),1))
+        self.job_search()
+        time.sleep(round(random.uniform(2,3),1))
+        self.filter()
+        time.sleep(round(random.uniform(2,3),1))
+        self.find_offers()
+        time.sleep(round(random.uniform(2,3),1))
+        self.close_session()
+        
 if __name__ == "__main__":
 
     with open("config.json") as config_file:
         data = json.load(config_file)
     
     bot = EasyApplyLinkedIn(data)
-    bot.login_linkedin()
-    time.sleep(round(random.uniform(2,3),1))
-    bot.job_search()
-    time.sleep(round(random.uniform(2,3),1))
-    bot.filter()
-    time.sleep(round(random.uniform(2,3),1))
-    bot.find_offers()
+    bot.apply()
